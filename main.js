c4740063c958bcf715d7ceb19ede3af419957aa4
alsonmachingauta06-lab <alsonmachingauta06@gmail.com>
2026-09-10 09:24:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1592,11 +1592,11 @@ case userMessage.startsWith('.setgpp'):
             case userMessage.startsWith('.spotify'):
                 await spotifyCommand(sock, chatId, message);
                 break;
-        
-         case 'play':
+        case userMessage.startsWith('.play'):
     await playCommand(sock, chatId, message);
     commandExecuted = true;
     break;
+           
     
             case userMessage.startsWith('.mp3'):
                 await playCommand(sock, chatId, message);
